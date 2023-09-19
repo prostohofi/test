70354793f4293cebaa7d95d45bbc16299fc1af7a
xofishacos <145482767+xofishacos@users.noreply.github.com>
2023-09-19 19:19:39 +0300

    Add files via upload

---
+++ tekst.py
@@ -0,0 +1,21 @@
+temperature =-41
+
+"""
+
+if temperature < 0:
+    print ("Захвати шапку")
+else:
+       if temperature < 20:
+            print ("Ничего не надо")
+       else:
+             print ("Захвати крем")
+"""
+
+if temperature < -40:
+    print("Захвати шубу")
+elif temperature < 0:
+    print ("Захвати шапку")
+elif temperature < 20:
+    print ("Ничего не надо")
+else:
+        print ("Захвати крем")
