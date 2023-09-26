3d9e66e9f43b321d089495c9c55c7287441d143e
xofishacos <145482767+xofishacos@users.noreply.github.com>
2023-09-26 19:43:03 +0300

    Add files via upload

---
+++ циклы.py
@@ -0,0 +1,10 @@
+laps = 0
+
+while laps < 10: # 10 кругов
+    laps += 1
+    print("пробежал", laps, "кругов")
+    if laps == 10:
+        continue
+    print("отдохнул")
+
+print ("треня окончена")
