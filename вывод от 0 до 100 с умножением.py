dd02e0df216d97f2b8a14f19a7436b85f4869e86
xofishacos <145482767+xofishacos@users.noreply.github.com>
2023-10-03 19:12:01 +0300

    Add files via upload

---
+++ вывод от 0 до 100 с умножением.py
@@ -0,0 +1,14 @@
+num = 0
+
+while num < 101:
+    if  num % 15  == 0:
+        print("физбаз")
+    elif num % 5 == 0:
+        print("физ")
+    elif  num % 3 == 0:
+        print("баз")
+    else:
+         print (num)
+    num += 1
+   
+    
